f6c68ad44ed943356fcada2161ec43d83dcc8845
DL-Bernardo <bvieira@digitalub.ao>
2026-09-09 09:46:12 +0000

    refactor(store): localize store descriptions to English preserving visual layout v16.0.1.0.5

--- dl_stock_barcode/__manifest__.py
+++ dl_stock_barcode/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Mobile Stock Barcode Scanner for Community',
-    'version': '16.0.1.0.4',
+    'version': '16.0.1.0.5',
     'summary': 'Zero Hardware Lock-in Mobile Barcode Scanning, Real-time Inventory & Fast Picking Automation',
         'description': """
         Professional Barcode Scanner & Picking Management for Odoo Community & Enterprise.
